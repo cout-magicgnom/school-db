17eafe14b761b0ccccad929fec96cf4e6c1467a3
Felipe melo <femanero12@gmail.com>
2026-05-22 11:36:49 -0300

    Update DLinkedList.java

--- src/apl2/DLinkedList.java
+++ src/apl2/DLinkedList.java
@@ -1,25 +1,7 @@
-// arquivo: src/apl2/DLinkedList.java
-/*
-	* construtor
-	* metodos:
-		
-*/
 
-//Arthur Candidio Palma 10743580
-//Felipe Melo 10741166
-//Kauê Cordeiro Abreu 10741435
 
 package apl2;
 
-// -- A classe DLinkedList (que pertence ao pacote apl2) deve implementar uma
-// lista duplamente encadeada. Os nós dessa lista são do tipo [da classe] Node.
-// -- A classe deve possuir dois nós especiais, head e tail, que são
-// referências para o primeiro e último nó da lista, respectivamente.
-// -- A classe deve possuir um contador de quantos nós existem na lista.
-// -- A classe deve sobrescrever (override) o método public String toString()
-// {...}, retornando uma string com o conteúdo da lista.
-// -- A classe deve implementar as operações a seguir, respeitando o
-// comportamento descrito em cada operação.
 
 public class DLinkedList {
 	private Node head;
@@ -27,7 +9,6 @@ public class DLinkedList {
 	private int size;
 
 
-	// TODO: Implementar a classe conforme o enunciado da atividade Apl2.
 	
 	//CONSTRUTOR
 	public DLinkedList() {
