b627270d5f46a3cc7055291b53569a1bc15c47c6
Felipe melo <femanero12@gmail.com>
2026-05-22 11:35:54 -0300

    Update MainApl2.java

--- src/MainApl2.java
+++ src/MainApl2.java
@@ -1,16 +1,3 @@
-//*************************** ATENÇÃO! ****************************
-// O método main() deve ser alterado somente nos comentários TODO.
-// Todas as outras instruções devem permanecer intactas e o código
-// deve funcionar conforme descrito no enunciado da atividade.
-//*************************** ATENÇÃO! ****************************
-// arquivo: src/MainApl2.java
-
-
-//Arthur Candidio Palma 10743580
-//Felipe Melo 10741166
-//Kauê Cordeiro Abreu 10741435
-
-// TODO: Listar todas as referências consultadas para solucionar a atividade.
 
 import apl2.*;
 
@@ -20,8 +7,6 @@ public class MainApl2 {
 		LinkedListOriginal list = new LinkedListOriginal();
 
 		
-		// TODO: Carregar o conteúdo do arquivo "dados.txt" e adicionar cada linha como um nó na LinkedListOriginal list.
-
 		String info_dados = "";
 
 		try {
@@ -95,13 +80,9 @@ public class MainApl2 {
 		System.out.println(">>>>>>>>>> aboveAverageList.clear() >>>>>>>>>>\n" + aboveAverageList + "\n<<<<<<<<<< aboveAverageList.clear() <<<<<<<<<<\n");
 
 		DLinkedList testList = new DLinkedList();
-		// TODO: Inserir um nó no início da lista testList com os dados ("ABC", "John Doe", 4.7f).
 		testList.insert("ABC", "John Doe", 4.7f);
-		// TODO: Inserir um nó no final da lista testList com os dados ("XYZ", "Jane Doe", 9.9f).
 		testList.append("XYZ", "Jane Doe", 9.9f);
-		// TODO: Inserir um nó no início da lista testList com os dados ("321", "Test", 2.3f).
 		testList.insert("321", "Test", 2.3f);
-		// TODO: Inserir um nó no final da lista testList com os dados ("Nothing", "Yada yada yada", 99.9f).
 		testList.append("Nothing", "Yada yada yada", 99.9f);
 
 		System.out.println(">>>>>>>>>> testList >>>>>>>>>>\n" + testList  + "\n<<<<<<<<<< testList <<<<<<<<<<\n");
@@ -116,13 +97,9 @@ public class MainApl2 {
 		System.out.println(">>>>>>>>>> testList >>>>>>>>>>\n" + testList  + "\n<<<<<<<<<< testList <<<<<<<<<<\n");
 		System.out.println("testList.getHead(): " + testList.getHead());
 		System.out.println("testList.getTail(): " + testList.getTail() + '\n');
-		// TODO: Inserir um nó no início da lista testList com os dados ("qwerty", "QWERTY", 1.2f).
 		testList.insert("qwerty", "QWERTY", 1.2f);
-		// TODO: Inserir um nó no final da lista testList com os dados ("WASD", "wasd", 3.4f).
 		testList.append("WASD", "wasd", 3.4f);
-		// TODO: Inserir um nó no início da lista testList com os dados ("ijkl", "IJKL", 5.6f).
 		testList.insert("ijkl", "IJKL", 5.6f);
-		// TODO: Inserir um nó no final da lista testList com os dados ("1234", "Um Dois Tres Quatro", 7.8f).
 		testList.append("1234", "Um Dois Tres Quatro", 7.8f);
 		System.out.println(">>>>>>>>>> testList >>>>>>>>>>\n" + testList  + "\n<<<<<<<<<< testList <<<<<<<<<<\n");
 		testList.clear();
