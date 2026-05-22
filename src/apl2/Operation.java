ebeb35d54dff3fbbe02a3538bde257123492d503
Felipe melo <femanero12@gmail.com>
2026-05-22 11:37:40 -0300

    Refactor Operation class and remove comments

--- src/apl2/Operation.java
+++ src/apl2/Operation.java
@@ -1,26 +1,7 @@
-//*************************** ATENÇÃO! *****************************
-// As assinaturas dos métodos desta classe não devem ser alteradas!
-//*************************** ATENÇÃO! *****************************
-// arquivo: src/apl2/Operation.java
-
-
-//Arthur Candidio Palma 10743580
-//Felipe Melo 10741166
-//Kauê Cordeiro Abreu 10741435
-
 package apl2;
 
 public class Operation {
 
-	/**
-	 * <p>Recebe como parâmetro uma lista encadeada do tipo {@code LinkedListOriginal}, sendo que os nós da lista estão
-	 * populados com o conteúdo da base de dados original (conteúdo do arquivo dados.txt).</p>
-	 * <p>A operação {@code map()} deve mapear os dados originais para uma lista encadeada do tipo {@code DLinkedList} e
-	 * retornar a referência da {@code DLinkedList} que possui os dados mapeados para a nova estrutura usada pelo sistema de notas.</p>
-	 * 
-	 * @param original Base de dados original carregada em uma {@code LinkedListOriginal}.
-	 * @return Uma nova {@code DLinkedList} que contém o mapeamento da coleção de dados {@code original} para a nova estrutura usada pelo sistema de notas. 
-	 */
 	public static DLinkedList map(final LinkedListOriginal original) {
 		DLinkedList list = new DLinkedList();
 		NodeOriginal aux = original.getHead();
