656d673b56d911f14905bebbaecb4884595d3e40
Kauê Cordeiro Abreu <mobsters430@gmail.com>
2026-05-25 16:29:19 -0300

    Reintroduce Node class with attributes and methods
    Restored the Node class with appropriate attributes and methods.

--- src/apl2/Node.java
+++ src/apl2/Node.java
@@ -1,6 +1,21 @@
+// arquivo: src/apl2/Node.java
+
+//Arthur Candidio Palma 10743580
+//Felipe Melo 10741166
+//Kauê Cordeiro Abreu 10741435
+
+
 package apl2;
 
-
+// -- A classe Node (que pertence ao pacote apl2) deve conter os atributos que
+// representam a nova versão dos dados de uma pessoa, conforme descrito no
+// enunciado da atividade Apl2.
+// -- A classe deve conter os construtores apropriados, assim como os métodos
+// getters e setters.
+// -- A classe também representa um nó que é usado na implementação da lista
+// duplamente encadeada (classe DLinkedList).
+// -- A classe deve sobrescrever (override) o método public String toString()
+// {...}, retornando uma string com os valores dos atributos da classe.
 
 public class Node{
     private Node Right;
